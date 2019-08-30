17b3ae167969964d203946e7c5b8f3cf80e2f4eb
DarrenW <sdauwangzh@163.com>
2019-08-31 00:05:49 +0800

    修改处理

--- new_file.py
+++ new_file.py
@@ -9,15 +9,11 @@ from workflow import Workflow3, web
 
 def createNew(wf):
     query = sys.argv[1]
-    if query.endswith('/'):    
-        path = os.popen("osascript ./getPath.scpt")
-        for item in path:
-            item = re.split("\n",item)[0]
-            query = query[:-1]
-            filePath = item+query
-            wf.add_item(title=u"创建文件-->"+query, subtitle="文件路径-->"+item+"/"+query,arg=filePath,valid=True)
-    else:
-        wf.add_item(title=u"输入文件名,并以'/'结束")
+    path = os.popen("osascript ./getPath.scpt")
+    for item in path:
+        item = re.split("\n",item)[0]
+        filePath = item+query
+        wf.add_item(title=u"创建文件-->"+query, subtitle="文件路径-->"+item+"/"+query,arg=filePath,valid=True)
     wf.send_feedback()
 
 if __name__ == "__main__":
